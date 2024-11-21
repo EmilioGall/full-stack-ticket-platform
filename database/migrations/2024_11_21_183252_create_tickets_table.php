d56d6aa503d177b059a25eb9c231728396189e44
Emilio Gallo <emilio.gallo17@gmail.com>
2024-11-21 19:35:50 +0100

    feat: adjusted Migration - create_tickets_table.php

--- database/migrations/2024_11_21_183252_create_tickets_table.php
+++ database/migrations/2024_11_21_183252_create_tickets_table.php
@@ -14,15 +14,23 @@ return new class extends Migration
         Schema::create('tickets', function (Blueprint $table) {
 
             $table->id();
-    
-            $table->foreignId('category_id')->constrained()->onDelete('cascade');
-            $table->foreignId('operator_id')->constrained()->onDelete('cascade');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
             $table->string('title');
             $table->text('description');
             $table->enum('status', ['Assigned', 'InProgress', 'Closed']);
-    
+
             $table->timestamps();
+
+            //foreign keys
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->cascadeOnDelete();
+
+            $table->unsignedBigInteger('operator_id');
+            $table->foreign('operator_id')->references('id')->on('operators')->cascadeOnDelete();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+
         });
     }
 
@@ -31,7 +39,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        
+
         Schema::dropIfExists('tickets');
     }
 };
